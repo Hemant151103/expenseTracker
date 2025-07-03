bd0ac5355be6846b87f78be7c81abea27844152f
Hemant151103 <hemantchaudhari3003@gmail.com>
2025-07-03 09:22:54 +0530

    Add project on git

---
+++ src/main/java/com/mycompany/expense_tracker/Expense_tracker.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.expense_tracker;
+
+/**
+ *
+ * @author Sanjivani
+ */
+public class Expense_tracker {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
